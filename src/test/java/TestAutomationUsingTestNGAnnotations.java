446b08ce47821ab5836f7ba96d63d5733d090669
Kevin <kevintervalon@gmail.com>
2025-06-24 15:00:43 -0600

    New Selenium Code changes

--- src/test/java/TestAutomationUsingTestNGAnnotations.java
+++ src/test/java/TestAutomationUsingTestNGAnnotations.java
@@ -1,17 +1,16 @@
-
-import org.junit.Test;
-import org.junit.After;
-import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class TestAutomationUsingTestNGAnnotations {
     WebDriver driver;
 
-    @Before
+    @BeforeTest
     public void setup() {// driver setup
         System.setProperty("WebDriver.Chrome.Driver","C:\\Program Files\\Google\\Chrome\\Application");
         driver = new ChromeDriver();
@@ -23,7 +22,7 @@ public class TestAutomationUsingTestNGAnnotations {
         driver.findElement(By.name("search")).sendKeys("Selenium Webdriver");
     }
 
-    @After
+    @AfterTest
     public void teardown() {//quit driver
         driver.quit();
     }
